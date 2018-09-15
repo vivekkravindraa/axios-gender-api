21650286ec9941f434a189fad302ed8a6c4f31bf
vivek <vivek.vkit@gmail.com>
2018-09-15 21:36:38 +0530

    createIndex - fixed

--- config/db.js
+++ config/db.js
@@ -2,5 +2,6 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://localhost:27017/gender', {useNewUrlParser: true});
+mongoose.set('useCreateIndex', true);
 
 module.exports = mongoose;
